efd5087c7229ad5490d287e0ca22dcb9513ab053
OnkelJuls <62448764+OnkelJuls@users.noreply.github.com>
2025-01-23 11:49:13 +0100

    add human choice and initialize scores

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
     let choice =  Math.floor(Math.random() * 3)
     if (choice == 0){
@@ -9,4 +12,18 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+
+
+function getHumanChoice(){
+    let choice = prompt("Choose one: 0 = Rock, 1 = Paper, 2 = Scissors");
+    if (choice == 0){
+        return console.log("Rock");
+    } else if (choice == 1){
+        return console.log("Paper");
+    } else {
+        return console.log("Scissors");
+    }
+}
+
+getHumanChoice();
+getComputerChoice();
